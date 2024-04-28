bb0960db8357cb6ef89cd7dc2cbf6db5469cd1a0
Gabriel <gabrielvitor.frasao@gmail.com>
2024-04-28 20:03:13 -0300

    refactor: decrease status legend font size

--- src/components/StatusLegend.tsx
+++ src/components/StatusLegend.tsx
@@ -1,6 +1,6 @@
 export const StatusLegend = () => {
 	return (
-		<div className="absolute bottom-1 left-1/2 -translate-x-1/2 flex gap-2 px-1 bg-zinc-950 text-zinc-300 text-[9px] rounded-full">
+		<div className="absolute bottom-1 left-1/2 -translate-x-1/2 flex gap-2 px-1 bg-zinc-950 text-zinc-300 text-[8px] rounded-full">
 			<span>
 				<span className="inline-block w-1 h-1 mr-px rounded-full bg-emerald-500" />{" "}
 				Have some experience
